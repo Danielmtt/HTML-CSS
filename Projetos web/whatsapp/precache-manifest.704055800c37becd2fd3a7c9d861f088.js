ebf7799a12f0d08d8165ec11007230e1688946b6
Daniel Mota <daniel.mota@iteris.com.br>
2020-10-04 18:41:06 -0300

    aplicacao react

---
+++ Projetos web/whatsapp/precache-manifest.704055800c37becd2fd3a7c9d861f088.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0116a51be74927b6c443d67b025f465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03aa6bc42ee4db5dccd3",
+    "url": "/static/css/main.52b2109e.chunk.css"
+  },
+  {
+    "revision": "b264c8f5a94e21a82065",
+    "url": "/static/js/2.86b3435c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86b3435c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03aa6bc42ee4db5dccd3",
+    "url": "/static/js/main.2e683f0b.chunk.js"
+  },
+  {
+    "revision": "42491e563a24762e1297",
+    "url": "/static/js/runtime-main.245abe94.js"
+  },
+  {
+    "revision": "bfca0bd1bc14972cd346615cc8b08db7",
+    "url": "/static/media/nenhumContatoSelecionado.bfca0bd1.png"
+  },
+  {
+    "revision": "49ce7e49355c1135c0e1993360dd6b17",
+    "url": "/static/media/wallpaper_areamensagem.49ce7e49.png"
+  }
+]);
